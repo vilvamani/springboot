30efec5773bf37ecc21fa1ceb3d906d7b33baf0a
vilvamani.arumugam <vilvamani007@gmail.com>
2020-11-23 13:58:34 +0530

    send notification to slack

--- infra/K8s-jenkinsfile-new.groocy.groovy
+++ infra/K8s-jenkinsfile-new.groocy.groovy
@@ -9,32 +9,27 @@ jenkins_common_branch = "develop"
 jenkins_common_repo_url = "https://github.com/vilvamani/jenkins_common_library.git"
 jenkins_common_checkout_dir = "jenkins_library"
 jenkins_common_file = "jenkins_common_library.groovy"
-jenkins_slack_channel = "infra-development"
-
-colorBlue = '#0000FF'
-colorGreen = '#00FF00'
-colorRed = '#FF0000'
 
 params = [
     branch_checkout_dir: 'service',
+    service: 'springboot-microservice',
     branch: 'master',
     repo_url: 'https://github.com/vilvamani/springboot.git',
+    dockerRepoName: 'vilvamani007',
+    dockerImageName: 'sprintboot',
+    kubeDeploymentFile: './infra/k8s-deployment.yaml',
+    kubeServiceFile: './infra/k8s-service.yaml',
+    jenkins_slack_channel: "infra-development",
     skip_unit_test: false,
     skip_integration_test: true,
     skip_sonar: false,
     skip_artifactory: false,
     skip_docker_push: false,
     skip_kubernetes_deployment: false,
-    dockerRepoName: 'vilvamani007',
-    dockerImageName: 'sprintboot',
-    kubeDeploymentFile: './infra/k8s-deployment.yaml',
-    kubeServiceFile: './infra/k8s-service.yaml'
+    skip_notification: false
 ]
 
 node('jenkins-slave') {
-    //def mvnHome = tool 'M3'
-    //env.PATH = "${mvnHome}/bin:${env.PATH}"
-
     step([$class: 'WsCleanup'])
     jenkinsLibrary = loadJenkinsCommonLibrary()
     jenkinsLibrary.defaultConfigs(params)
@@ -43,12 +38,9 @@ node('jenkins-slave') {
             jenkinsLibrary.mavenSpingBootBuild(params)
         } catch (Exception err) {
             currentBuild.result = 'FAILURE'
-
-            jenkinsLibrary.sendToSlack(colorRed, "FAILURE", 'sprintboot', jenkins_slack_channel, params.branch)
-
             throw err
         } finally {
-            jenkinsLibrary.sendToSlack(colorBlue, "SUCCESS", 'sprintboot', jenkins_slack_channel, params.branch)
+            jenkinsLibrary.sendSlack(params)
         }
     }
 }
